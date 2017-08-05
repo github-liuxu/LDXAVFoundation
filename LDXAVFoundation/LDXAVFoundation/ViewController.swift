4e578fb4841368cf876a20eb48d0885f21cc99fe
刘东旭 <chuyang009@163.com>
2017-08-06 03:39:22 +0800

    init work

---
+++ LDXAVFoundation/LDXAVFoundation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LDXAVFoundation
+//
+//  Created by 刘东旭 on 2017/8/6.
+//  Copyright © 2017年 刘东旭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
